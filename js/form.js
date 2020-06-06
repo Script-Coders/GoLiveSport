7ad8a87d055a23e91fcdefa5ab85e9ee86f53088
thaer <thaerkhaled13@gmail.com>
2020-06-07 00:27:04 +0300

    creat the element to the select according to the bmi

--- js/form.js
+++ js/form.js
@@ -7,8 +7,17 @@ function bmi(){
     // Math.pow(sheight,2);
     var bmiC=document.getElementById('result');
     bmiC.textContent=bmi.toFixed(2);
+    
     if(bmi < 18.5){
         bmiC.textContent= "your BMI is: "+bmi.toFixed(2)+ " and you are Underweight";
+
+        //creat element in select drop down list
+        var z = document.createElement("option");
+      z.setAttribute("value", "under Weight");
+      var t = document.createTextNode("yoga");
+      z.appendChild(t);
+      
+      document.getElementById("mySelect").appendChild(z);
       }else if (bmi < 25){
         bmiC.textContent="your BMI is: "+bmi.toFixed(2)+ " and you are Normal";
       }else if (bmi < 30){
@@ -18,14 +27,23 @@ function bmi(){
       }else{
         bmiC.textContent="your BMI is: "+bmi.toFixed(2)+ " and you are Obese";
       }
+//creat a new select list  
+      // var x = document.createElement("SELECT");
+      // x.setAttribute("id", "mySelect");
+      // document.body.appendChild(x);
+    
+      
    
     }
+  
+
 //to clear the form
     function resetForm() {
     document.getElementById("fname").value=""; 
     document.getElementById("height").value=""; 
     document.getElementById("wieght").value="";
     document.getElementById("result").value="your BMI and your suggstion for the class"; 
+    document.getElementById("mySelect").value="";
     }
 //git  class from local storag
     // function addToCart(){
