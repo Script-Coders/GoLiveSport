72eadd2ce623237444637b9416cd184ba84190f2
MohammedGhafri <eng.m.ghafri@gmail.com>
2020-06-08 03:56:49 +0300

    Dynamic options for form

--- js/form.js
+++ js/form.js
@@ -1,54 +1,146 @@
 //calculate the bmi
-function bmi(){
 
-    var sheight=parseFloat(document.getElementById('height').value);
-    var sweight=parseFloat(document.getElementById('wieght').value);
-    var bmi=sweight/(sheight/100*sheight/100);
-    // Math.pow(sheight,2);
-    var bmiC=document.getElementById('result');
-    bmiC.textContent=bmi.toFixed(2);
-    
-    if(bmi < 18.5){
-        bmiC.textContent= "your BMI is: "+bmi.toFixed(2)+ " and you are Underweight";
+'use strict';
+var bmiC = document.getElementById('result');
+var indexCase = ['Underweight', 'Normal', 'Overweight', 'Obese'];
+aclass1=['Yoga'];
+aclass2=['Yoga','Boxing'];
+aclass3=['Body Builing','Running', 'Aikido'];
+aclass4=['Body Builing','Running', 'Aikido','Cardio']
+var aclass1,aclass2,aclass3,aclass4;
+var classType = [aclass1, aclass2, aclass3, aclass4];
 
-        //creat element in select drop down list
-        var z = document.createElement("option");
-      z.setAttribute("value", "under Weight");
-      var t = document.createTextNode("yoga");
-      z.appendChild(t);
-      
-      document.getElementById("mySelect").appendChild(z);
-      }else if (bmi < 25){
-        bmiC.textContent="your BMI is: "+bmi.toFixed(2)+ " and you are Normal";
-      }else if (bmi < 30){
-        bmiC.textContent="your BMI is: "+bmi.toFixed(2)+ " and you are Overweight";
-        //array class fit him will be add to the list
-        
-      }else{
-        bmiC.textContent="your BMI is: "+bmi.toFixed(2)+ " and you are Obese";
-      }
-//creat a new select list  
-      // var x = document.createElement("SELECT");
-      // x.setAttribute("id", "mySelect");
-      // document.body.appendChild(x);
-    
-      
-   
-    }
+function Class(name,sport){
+  this.nameClass=name;
+  this.sport=sport;
+  this.options=[];
+  Class.all.push(this);
+}
+
+Class.all=[];
+
+
+Class.prototype.optionsArray=function(i){
+  this.options=getArray(i);
+}
+
+
+for(var i=0;i<indexCase.length;i++){
+  new Class(indexCase[i],"sport");
+  Class.all[i].optionsArray(i);
+  console.log(Class.all[i]);
+}
+
+function getArray(i){
+  var bArray=[];
   
+  bArray=classType[i];
+  // console.log(classType[i]);
+  // console.log(bArray);
+  return bArray;
+      }
+// console.log(classType[1]);
+
+function bmi() {
+  event.preventDefault();
+  var sheight = parseFloat(document.getElementById('height').value);
+  var sweight = parseFloat(document.getElementById('wieght').value);
+  
+  var bmiCal = sweight / (sheight / 100 * sheight / 100);
+  
+  // Math.pow(sheight,2);
+  bmiC.textContent = bmiCal.toFixed(2);
+  
+  
+  if (bmiCal < 18.5) {
+    var var1 = 0;
+    creatoptions(bmiCal, var1);
+    
+    //creat element in select drop down list
+    var z = document.createElement("option");
+    z.setAttribute("value", "under Weight");// will we use Value=under Weight ? if, where and how ?
+    var t = document.createTextNode("yoga");// 
+    z.appendChild(t);
+    // z.textContent="Yoga";// this can be used instead of the above two lines
+    
+    
+    
+    
+  } else if (bmiCal < 25) {
+    var var1 = 1;
+    creatoptions(bmiCal, var1);
+    
+  } else if (bmiCal < 30) {
+    var var1 = 2;
+    creatoptions(bmiCal, var1);
+    
+  } else {
+    var var1 = 3;
+    creatoptions(bmiCal, var1);
+    
+  }
+
+  //creat a new select list  
+  // var x = document.createElement("SELECT");
+  // x.setAttribute("id", "mySelect");
+  // document.body.appendChild(x);
+  
+  var formOrigin=document.getElementById('form1');
+
+ 
+  
+  resetForm(); //To reset the Form
+  console.log( document.getElementById('form1'));
+}
+
 
 //to clear the form
-    function resetForm() {
-    document.getElementById("fname").value=""; 
-    document.getElementById("height").value=""; 
-    document.getElementById("wieght").value="";
-    document.getElementById("result").value="your BMI and your suggstion for the class"; 
-    document.getElementById("mySelect").value="";
-    }
+function resetForm() {
+  // document.getElementById('form1').reset(); // we can use this instrction instead of separated for each tag in the form instructions
+  // console.log("reset");
+  // document.getElementById("fname").value = "";
+  // document.getElementById("height").value = "";
+  // document.getElementById("wieght").value = "";
+  // document.getElementById("result").value = "your BMI and your suggstion for the class";
+  // document.getElementById("mySelect").value = "";
+}
+
+function creatoptions(bmicalculated, var1) {// To creat options to select tag and show the BMI result
+  
+  bmiC.textContent = `your BMI is: ${bmicalculated.toFixed(2)} and you are  ${indexCase[var1]}`;
+  var select=document.createElement('select');
+  select.setAttribute('id','mySelect');
+  // select.setAttribute('name','class fit you');
+  document.getElementById('formF').appendChild(select);
+  document.getElementById("mySelect").textContent=null;
+  //creat element in select drop down list
+  // var z = document.createElement("option");
+  // z.setAttribute("value", `${classType[var1]}`);// will we use Value=under Weight ? if, where and how ?
+  // // var t = document.createTextNode("yoga");// 
+  // // z.appendChild(t);
+  // // console.log(z);
+  // z.textContent=`${classType[var1]}`;// this can be used instead of the above two lines
+ 
+// // var indexCase = ['Underweight', 'Normal', 'Overweight', 'Obese'];
+// console.log(Class.all[0].options.length);
+for(var i=0;i<Class.all[var1].options.length;i++){
+  var z = document.createElement("option");
+  z.setAttribute("value", `${Class.all[var1].options[i]}`);
+  z.textContent=`${Class.all[var1].options[i]}`;
+  document.getElementById("mySelect").appendChild(z);
+}
+// var aa=document.getElementsByTagName('MAIN');
+// console.log(aa);
+// var bb=document.createElement('p').textContent="mmmmkmkdjfkdjljs";
+// aa.appendChild(bb);
+
+}
 //git  class from local storag
     // function addToCart(){
-        
+
     //     var cartItems = JSON.parse(localStorage.getItem('class')) || [];
-       
+
     //     console .log(cartItems);
     // }
+
+    
