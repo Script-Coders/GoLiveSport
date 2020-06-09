d497d7ddde578d9923ef7fb3daf5f788060ee3bf
dana <abbadidana91@gmail.com>
2020-06-09 08:28:19 +0300

    fixing logo

--- js/app.js
+++ js/app.js
@@ -127,18 +127,18 @@ function showSlides(n) {
 }
 
 
-// var slideIndex2 = 0;
-// showSlides2();
+var slideIndex2 = 0;
+showSlides2();
 
-// function showSlides2() {
-//   var i;
-//   var slides2 = document.getElementsByClassName("mySlides");
-//   for (i = 0; i < slides2.length; i++) {
-//     slides2[i].style.display = "none";
-//   }
-//   slideIndex2++;
-//   if (slideIndex2 > slides2.length) {slideIndex2 = 3}
-//   slides2[slideIndex2 -1].style.display = "block";
+function showSlides2() {
+  var i;
+  var slides2 = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides2.length; i++) {
+    slides2[i].style.display = "none";
+  }
+  slideIndex2++;
+  if (slideIndex2 > slides2.length) {slideIndex2 = 3}
+  slides2[slideIndex2 -1].style.display = "block";
  
-//   setTimeout(showSlides2, 5000); // Change image every 2 seconds
-// }
+  setTimeout(showSlides2, 5000); // Change image every 2 seconds
+}
